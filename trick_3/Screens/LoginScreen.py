c046cebd00542506c9e1cafd91882b23e8ec93b6
dimaberezovskyi <dmberezovsky@gmail.com>
2023-08-23 13:55:47 +0300

    trick 3, base framework structure

--- trick_3/Screens/LoginScreen.py
+++ trick_3/Screens/LoginScreen.py
@@ -1,5 +1,9 @@
 from BaseScreen import Base
-class LoginScreem(Base):
+from appium.webdriver.common.mobileby import MobileBy
+
+
+class LoginScreen(Base):
+    ADD_CITY = (MobileBy.ACCESSIBILITY_ID, 'Login btn')
 
     def __init__(self, driver):
         super().__init__(driver)
